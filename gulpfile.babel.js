a5bb88880fdb1c97401f51c616050c35a86a5428
Amin Shazrin <aminshazrin@yahoo.com>
2020-01-22 16:47:08 +0800

    Adjust widgetEditor.html to use extends method of my own tableSchemas. Also options.schema change to options.dynamicTableSchemas due to conflict existing options called schema

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,7 +17,6 @@ var src = "src/"
 
 var JS = [
     { src: 'src/js/lean/', name: 'lean', dest: 'lib/modules/dynamic-table-widgets/public/js' },
-    { src: 'src/js/lean/', name: 'jQueryLean', dest: 'lib/modules/dynamic-table-widgets/public/js' },
     { src: 'src/js/utils/', name: 'utils', dest: 'public/js' },
     { src: 'src/js/extends/', name: 'customCodeEditor', dest: 'public/js/extends'}
 ];
