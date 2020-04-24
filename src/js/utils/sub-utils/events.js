95a04c8e36a939cc3b8c8dbe8ad558c7931f449a
Amin Shazrin <aminshazrin@yahoo.com>
2020-04-25 07:48:41 +0800

    get widgetId instead of pieceId

--- src/js/utils/sub-utils/events.js
+++ src/js/utils/sub-utils/events.js
@@ -1,6 +1,7 @@
 let events = function(self, options) {
     self.allListener = function () {
         apos.on('widgetTrashed', function ($widget) {
+            let widgetId = $widget.data().aposWidgetId;
             if ($widget.data() && $widget.data().aposWidget === 'dynamic-table') {
                 let pieceId = apos.modules['dynamic-table-widgets'].getData($widget).dynamicTableId;
                 self.removeUrlsApi({
@@ -12,9 +13,9 @@ let events = function(self, options) {
                     }
                     // Delete Browser widget options table
                     if (apos.dynamicTableWidget) {
-                        delete apos.dynamicTableWidget.tabulator[pieceId];
+                        delete apos.dynamicTableWidget.tabulator[widgetId];
                     } else if (apos.dynamicTableLean) {
-                        delete apos.dynamicTableLean[pieceId];
+                        delete apos.dynamicTableLean[widgetId];
                     }
                     return apos.utils.log('Successful remove widget location.');
                 })
